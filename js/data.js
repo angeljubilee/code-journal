16a41b3ab65902c6ceb7f96da0d068ac408d423c
Angel Lee <angeljubilee@gmail.com>
2021-07-13 16:47:38 -0700

    Fixed errors and cleaned up styling

--- js/data.js
+++ js/data.js
@@ -12,60 +12,9 @@ if (prevData) {
   data = JSON.parse(prevData);
 }
 
-var $ul = document.querySelector('.journal-entries');
-if (!data.entries.length) {
-  $ul.textContent = 'No entries have been recorded.';
-} else {
-  $ul.textContent = '';
-}
-
-var $views = document.querySelectorAll('.view');
-$views.forEach(node => {
-  if (node.getAttribute('data-view') === data.view) {
-    node.className = 'view';
-  } else {
-    node.className = 'view hidden';
-  }
-});
-
-for (var i = 0; i < data.entries.length; i++) {
-  $ul.appendChild(getDOM(data.entries[i]));
-}
-
 window.addEventListener('beforeunload', handleLocalStorage);
 
 function handleLocalStorage(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataJSON);
-}
-
-function getDOM(entry) {
-  var $node = document.createElement('li');
-  $node.className = 'journal-entry';
-
-  var $row = document.createElement('div');
-  $row.className = 'row';
-  $node.appendChild($row);
-
-  var $column1 = document.createElement('div');
-  $column1.className = 'column-half';
-  $row.appendChild($column1);
-
-  var $img = document.createElement('img');
-  $img.setAttribute('src', entry.imgUrl);
-  $column1.appendChild($img);
-
-  var $column2 = document.createElement('div');
-  $column2.className = 'column-half';
-  $row.appendChild($column2);
-
-  var $title = document.createElement('h2');
-  $title.textContent = entry.title;
-  $column2.appendChild($title);
-
-  var $notes = document.createElement('p');
-  $notes.textContent = entry.notes;
-  $column2.appendChild($notes);
-
-  return $node;
 }
--- js/main.js
+++ js/main.js
@@ -7,11 +7,29 @@ var $form = document.querySelector('.newEntry');
 var $newEntryView = document.querySelector('.new-button');
 var $navEntries = document.querySelector('.nav-entries');
 var $views = document.querySelectorAll('.view');
+var $ul = document.querySelector('.journal-entries');
 
 $photoUrl.addEventListener('input', handleImgUrl);
 $form.addEventListener('submit', handleSubmit);
 $newEntryView.addEventListener('click', showNewForm);
 $navEntries.addEventListener('click', showEntries);
+
+if (!data.entries.length) {
+  $ul.textContent = 'No entries have been recorded.';
+} else {
+  $ul.textContent = '';
+}
+for (var i = 0; i < data.entries.length; i++) {
+  $ul.appendChild(getDOM(data.entries[i]));
+}
+
+$views.forEach(node => {
+  if (node.getAttribute('data-view') === data.view) {
+    node.className = 'view';
+  } else {
+    node.className = 'view hidden';
+  }
+});
 
 function handleImgUrl(event) {
   $img.setAttribute('src', event.target.value);
@@ -53,4 +71,35 @@ function showEntries(event) {
     }
   });
   data.view = 'entries';
+}
+
+function getDOM(entry) {
+  var $node = document.createElement('li');
+  $node.className = 'journal-entry';
+
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  $node.appendChild($row);
+
+  var $column1 = document.createElement('div');
+  $column1.className = 'column-half';
+  $row.appendChild($column1);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entry.imgUrl);
+  $column1.appendChild($img);
+
+  var $column2 = document.createElement('div');
+  $column2.className = 'column-half';
+  $row.appendChild($column2);
+
+  var $title = document.createElement('h2');
+  $title.textContent = entry.title;
+  $column2.appendChild($title);
+
+  var $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  $column2.appendChild($notes);
+
+  return $node;
 }
