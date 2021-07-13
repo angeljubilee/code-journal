d584bfce0450df57d8c69366e73958163b78cb97
Angel Lee <angeljubilee@gmail.com>
2021-07-13 11:59:34 -0700

    In progress

--- js/data.js
+++ js/data.js
@@ -12,9 +12,44 @@ if (prevData) {
   data = JSON.parse(prevData);
 }
 
+for (var i = 0; i < data.entries.length; i++) {
+  getDOM(data.entries[i]);
+}
+
 window.addEventListener('beforeunload', handleLocalStorage);
 
 function handleLocalStorage(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+function getDOM(entry) {
+  var $node = document.createElement('li');
+  $node.className = 'journal-entry';
+
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  $node.appendChild($row);
+
+  var $column1 = document.createElement('div');
+  $column1.className = 'column-half';
+  $row.appendChild($column1);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entry.imgUrl);
+  $column1.appendChild($img);
+
+  var $column2 = document.createElement('div');
+  $column2.className = 'column-half';
+  $row.appendChild($column2);
+
+  var $title = document.createElement('h2');
+  $title.textContent = entry.title;
+  $column2.appendChild($title);
+
+  var $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  $column2.appendChild($notes);
+
+  return $node;
 }
