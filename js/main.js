d49cd0b601e4b9a746d8902f5a741a1a78f43442
Angel Lee <angeljubilee@gmail.com>
2021-07-12 17:16:29 -0700

    Verify that each new entry receives an incrementing entryId.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#imgURL');
+$photoUrl.addEventListener('input', handleImgUrl);
+var $img = document.querySelector('img');
+
+function handleImgUrl(event) {
+  $img.setAttribute('src', event.target.value);
+}
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var newEntry = {
+    title: $form.elements.title.value,
+    imgUrl: $form.elements.imgURL.value,
+    notes: $form.elements.notes.value
+  };
+  newEntry.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  $img.setAttribute('src', '');
+  data.entries.unshift(newEntry);
+  $form.reset();
+}
